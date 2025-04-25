778d4bd41642b5933c369e32a8712fc4a6aeb5c0
Avanti Singh <avantisingh19@gmail.com>
2025-04-25 15:36:41 -0500

    Initial Commit

---
+++ Navigation Stack/Navigation_StackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_StackApp.swift
+//  Navigation Stack
+//
+//  Created by Scholar on 4/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_StackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Navigation StackTests/Navigation_StackTests.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_StackTests.swift
+//  Navigation StackTests
+//
+//  Created by Scholar on 4/25/25.
+//
+
+import Testing
+@testable import Navigation_Stack
+
+struct Navigation_StackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
